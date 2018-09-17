1188d15929e1332199f6d041891746540f75e5d0
Verbol Xyz <verbol.xyz@gmail.com>
2018-09-17 14:57:50 +0200

    - finalisation

--- server.js
+++ server.js
@@ -1,36 +1,41 @@
-/**
- * TODO:
-    - Quand un client se connecte, il récupère la dernière Todolist connue du serveur
-    - Quand un client ajoute une tâche, celle-ci est immédiatement répercutée chez les autres clients
-    - Quand un client supprime une tâche, celle-ci est immédiatement supprimée chez les autres clients
- */
 
 var http = require("http");
 var express = require("express");
-var bodyParser = require("body-parser"); // Charge le middleware de gestion des paramètres
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
+var fs = require("fs");
 var app = express();
 var server = http.Server(app);
 var io = require("socket.io").listen(server);
 //on initialise la liste de todo à vide
 var todolist = [];
 
-app
-  //affiche la todolist
-  .get("/", (req, res) => {
-    res.render("page.ejs", { todolist: todolist });
-  })
- 
-  /* On redirige vers la todolist si la page demandée n'est pas trouvée 
+// on charge la page html client
+app.get("/", function(req, res) {
+  fs.readFile("views/client.html", "utf-8", function(error, content) {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(content);
+  });
+});
+
+/* On redirige vers la todolist si la page demandée n'est pas trouvée 
   .use(function(req, res, next) {
     res.redirect("/");
   });
 */
-  // on gere ici la socket
-  io.sockets.on("connection", (socket)=>{
-    socket.on("add todo",(newTodo)=>{
-      console.log("add todo de : "+newTodo)
-    })
-  })
+// on gere ici la socket
+io.sockets.on("connection", socket => {
+  // a chaque nouvelle connexion on envoi la todolist actuelle
+  io.emit("update todolist", todolist);
+
+  // ajout d'une nouvelle tache a la todo
+  socket.on("add todo", newTodo => {
+    todolist.push(newTodo);
+    io.emit("update todolist", todolist);
+  });
+  
+  // supression d'une todo
+  socket.on("delete todo", index => {
+    todolist.splice(index, 1);
+    io.emit("update todolist", todolist);
+  });
+});
 server.listen(8080);
